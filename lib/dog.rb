68b863fe11f2f6b182fe4c8121b78764db6d1f9b
Mazin <code.mazin.m@gmail.com>
2023-09-25 15:19:24 +1000

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,31 @@
 class Dog
 
+    attr_accessor :name, :breed, :id
+
+    def initialize(name:, breed:, id: nil)
+        @id = id
+        @name = name
+        @breed = breed
+    end
+
+    def self.drop_table
+        sql = <<-SQL
+            DROP TABLE IT EXISTS songs
+        SQL
+
+        DB[:conn].execute(sql)
+    end
+
+    def self.create_table
+        sql = <<-SQL
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        SQL
+
+        DB[:conn].execute(sql)
+    end
+
 end
